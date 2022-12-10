acb6a38f7a523cc1a6c59d97b532a4ea849c53f5
OmerFarukYilmaz-github <66321088+OmerFarukYilmaz-github@users.noreply.github.com>
2022-12-10 18:18:24 +0300

    fix: change lineRenderer material to sprites

--- Bump-Pop-Case/Assets/Scripts/BallController.cs
+++ Bump-Pop-Case/Assets/Scripts/BallController.cs
@@ -37,6 +37,8 @@ public class BallController : MonoBehaviour
     {
         if (!isActive) return;
 
+        DrawLine();
+
         if (currentState == states.idle && Touchscreen.current.press.isPressed)
         {
             currentState = states.aiming;
